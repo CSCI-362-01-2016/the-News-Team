5986f57e6d5504ac8b478cfaa02e25b941d8e15a
Matt Bell <bellmj@g.cofc.edu>
2016-10-25 03:57:15 -0700

    started output

--- scripts/RunAllTests.py
+++ scripts/RunAllTests.py
@@ -47,6 +47,31 @@ def executeTestCase():
 
 def writeOutputToHtml():
     return;
+def textToHtml():
+    inf = open("TextForHtml.txt","r")
+    outf = open("Output.html","w")
+    template = Template("""<!doctype html>
+
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+
+      <title>Files In Directory</title>
+
+    </head>
+
+    <body>
+        <h1>The Files In the Directory Are:</h1>
+      <p>$output</p>
+    </body>
+    </html>""")
+    files = "";
+    for line in inf:
+        files += line + "<br>"
+
+    outf.write(template.substitute(output=files))
+    inf.close()
+    outf.close()
 
 def loadOutputInBrowser():
     return;
