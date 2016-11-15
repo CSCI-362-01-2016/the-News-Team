7c8e574f48922701e9bddc091a4668cd4f97986c
Matthew <bellmj@g.cofc.edu>
2016-11-15 09:03:49 -0500

    asdf

--- scripts/testing.py
+++ scripts/testing.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 from project.youtube_dl import utils
-print(utils.sanitize_filename("this is a file name       to sanitize?}{[]\|#'$'",True,False))
+
+def main():
+    print("what" ,calc_percent(128,1024))
+def calc_percent(byte_counter, data_len):
+    if data_len is None:
+        return None
+    return float(byte_counter) / float(data_len) * 100.0
