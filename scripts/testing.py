db133e280adf88cc8340191fadafecf12e6778cd
Matt Bell <bellmj@g.cofc.edu>
2016-11-30 20:24:44 -0800

    cleaned up project

--- scripts/testing.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from project.youtube_dl import utils
-
-def main():
-    print("what" ,calc_percent(128,1024))
-def calc_percent(byte_counter, data_len):
-    if data_len is None:
-        return None
-    return float(byte_counter) / float(data_len) * 100.0
